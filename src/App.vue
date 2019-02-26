ffef1b043e6b1f550da17d5bc910e9588ae6d222
akramsmahmoud <akrammahmoud.web@gmail.com>
2019-02-26 23:26:19 +0100

    update (nested component)

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,27 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+<div>
+  <h1>{{ name }}</h1>
+  <kimo></kimo>
   </div>
 </template>
 
 <script>
+
+import comp from './comp.vue';
 export default {
+  components: {
+'kimo': comp
+  },
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      name: 'Do you?'
     }
   }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -56,5 +47,8 @@ li {
 
 a {
   color: #42b983;
+}
+h1{
+  color:red;
 }
 </style>
---
+++ src/comp.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <h1>Ay kalam <span>{{ msg }}</span></h1>
+    </div>    
+</template>
+
+
+<script>
+export default {
+    data(){
+        return{
+            msg: 'fady'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+h1{
+    color: aqua;
+    span{
+        color: brown;
+        font-weight: bold;
+    }
+}
+</style>
